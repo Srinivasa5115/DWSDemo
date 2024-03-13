fe0734fae4bf146e73d8eca1e5c4c17987d6529f
Srinivasa5115 <163157669+Srinivasa5115@users.noreply.github.com>
2024-03-13 19:00:48 +0530

    Notification changes added
    Notification changes added

---
+++ challenge/src/main/java/com/dws/challenge/service/FCMService.java
@@ -0,0 +1,10 @@
+package com.dws.challenge.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FCMService {
+    public void sendMessageToToken(NotificationService request) {
+        // notification sent logic
+    }
+}
